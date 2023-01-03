5ae97545c0c97c113ab7acd963778570b40cf6fa
Nicolás Londoño <nicolas.londono@veevart.com>
2023-01-03 14:13:29 -0500

    Add: dip exercise final

--- src/solid/05-dip-a.ts
+++ src/solid/05-dip-a.ts
@@ -1,10 +1,11 @@
 import { PostService } from "./05-dip-b";
-import { JsonDataBaseService, LocalDataBaseService } from "./05-dip-c";
+import { JsonDataBaseService, LocalDataBaseService, webApiPostsService } from "./05-dip-c";
 
 // Main
 (async () => {
 	// const provider = new LocalDataBaseService();
-	const provider = new JsonDataBaseService();
+	// const provider = new JsonDataBaseService();
+	const provider = new webApiPostsService();
 	const postService = new PostService(provider);
 
 	const posts = await postService.getPosts();
--- src/solid/05-dip-c.ts
+++ src/solid/05-dip-c.ts
@@ -29,3 +29,10 @@ export class JsonDataBaseService implements PostProvider {
 		return localPosts;
 	}
 }
+
+export class webApiPostsService implements PostProvider {
+	async getPosts(): Promise<Post[]> {
+		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+		return response.json();
+	}
+}
